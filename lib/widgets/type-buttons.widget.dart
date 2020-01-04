4d64c80e4f65777dcaa2500ac760e0a8f0eb3022
Jailton Júnior <jailton.junior94@outlook.com>
2020-01-04 13:37:15 -0300

    conclusão do curso

---
+++ lib/widgets/type-buttons.widget.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+import 'package:eggs/blocs/app.bloc.dart';
+import 'package:eggs/widgets/button.widget.dart';
+
+class TypeButtons extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    final bloc = Provider.of<AppBloc>(context);
+    return bloc.state == "stopped" ? buttons(bloc) : SizedBox();
+  }
+
+  Widget buttons(bloc) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceAround,
+      children: <Widget>[
+        Button(
+          label: "Soft",
+          callback: () {
+            bloc.select("soft");
+          },
+          selected: bloc.selected == "soft",
+        ),
+        Button(
+          label: "Medium",
+          callback: () {
+            bloc.select("medium");
+          },
+          selected: bloc.selected == "medium",
+        ),
+        Button(
+          label: "Hard",
+          callback: () {
+            bloc.select("hard");
+          },
+          selected: bloc.selected == "hard",
+        ),
+      ],
+    );
+  }
+}
